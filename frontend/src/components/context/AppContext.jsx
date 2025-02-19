4ecdb842bb4d484bb91b12f72699ab63b53a7bdb
RasulKhanGuttal <rizwandakhan@gmail.com>
2025-02-19 12:59:04 +0530

    Update AppContext.jsx

--- frontend/src/components/context/AppContext.jsx
+++ frontend/src/components/context/AppContext.jsx
@@ -10,7 +10,7 @@ export const AppContext = createContext();
 export const AppContextProvider = (props) => {
   axios.defaults.withCredentials = true;
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://mern-auth-backend-edq1.onrender.com";
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(false);
 
